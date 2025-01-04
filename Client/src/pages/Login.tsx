e6b06e538f28f9ad72f3e2ad0906650785f8ac6a
Suhani Sahu <sahusuhani14@gmail.com>
2025-01-04 13:52:48 +0530

    completed

--- Client/src/pages/Login.tsx
+++ Client/src/pages/Login.tsx
@@ -31,7 +31,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3000/api/auth/login", {
+      const response = await fetch("https://efficio-server.vercel.app/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
