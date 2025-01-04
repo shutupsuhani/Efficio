69514cd1e18c7e27e5522ada65798db2c0533e88
Suhani Sahu <sahusuhani14@gmail.com>
2025-01-04 15:07:33 +0530

    remove cors error

--- Server/index.js
+++ Server/index.js
@@ -14,11 +14,13 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors())
+
 app.use(cors({
   origin: "https://efficio-task.netlify.app/", // Allow only your frontend's URL
-  credentials: true,              // Allow cookies to be sent
+  /*credentials: true,              // Allow cookies to be sent
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers */
 }));
 
 // Routes
